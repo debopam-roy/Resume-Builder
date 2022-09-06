c8680ebd8d8d85d60369b4d9789788575f32d614
DebopamRoy <debopamroy.cs@gm.rkmvu.ac.in>
2022-09-07 03:15:47 +0530

    third commit

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -61,7 +61,7 @@ function Body() {
 
   return (
     <div className={styles.container}>
-      <p className={styles.heading}>Resume Builder</p>
+      <p className={styles.heading}>Build Your Resumes Here!</p>
       <div className={styles.toolbar}>
         <div className={styles.colors}>
           {colors.map((item) => (
